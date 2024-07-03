72a19057c32c885b0fd80f4b689eac2f1538ace3
kiwanacollins <collinzcalson@gmail.com>
2024-07-04 00:04:16 +0300

    ehhhhh

--- js/main.js
+++ js/main.js
@@ -81,3 +81,19 @@
     
 })(jQuery);
 
+
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+let slider = document.querySelector('.slider');
+
+next.addEventListener('click', function() {
+    let slides = document.querySelectorAll('.slides');
+    slider.appendChild(slides[0]);
+});
+
+prev.addEventListener('click', function() {
+    let slides = document.querySelectorAll('.slides');
+    slider.prepend(slides[slides.length - 1]);
+});
+
+
