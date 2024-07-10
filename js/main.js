bbfbcb42cebb0068046c02ba6b3aebaf9bc1e860
kiwanacollins <collinzcalson@gmail.com>
2024-07-10 13:17:40 +0300

    home page translation working

--- js/main.js
+++ js/main.js
@@ -86,15 +86,15 @@ let next = document.querySelector('.next');
 let prev = document.querySelector('.prev');
 let slider = document.querySelector('.slider');
 
-next.addEventListener('click', function() {
-    let slides = document.querySelectorAll('.slides');
-    slider.appendChild(slides[0]);
-});
+// next.addEventListener('click', function() {
+//     let slides = document.querySelectorAll('.slides');
+//     slider.appendChild(slides[0]);
+// });
 
-prev.addEventListener('click', function() {
-    let slides = document.querySelectorAll('.slides');
-    slider.prepend(slides[slides.length - 1]);
-});
+// prev.addEventListener('click', function() {
+//     let slides = document.querySelectorAll('.slides');
+//     slider.prepend(slides[slides.length - 1]);
+// });
 
 // Translation start
 
@@ -155,7 +155,7 @@ function translatePage(targetLang) {
     });
 }
 
-window.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const targetLangSelect = document.getElementById('targetLang');
     
     // Check for saved language in cookies
